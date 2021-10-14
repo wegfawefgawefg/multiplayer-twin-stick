6e62363f30cd2e496105ded3e41756776fe8eb6d
wegfawefgawefg <668es218pur@gmail.com>
2021-10-13 19:50:31 -0700

    fix server port

--- server.py
+++ server.py
@@ -51,7 +51,7 @@ class Server:
             #    break
 
 if __name__ == '__main__':
-    server = Server('localhost', 1234)
+    server = Server('localhost', 9999)
     threading.Thread(target=server.accept_connections).start()
     while True:
         time.sleep(1/60)
