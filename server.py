1bd4cb26fd28a3c242f14128983c4d714636800d
wegfawefgawefg <668es218pur@gmail.com>
2021-10-13 20:12:15 -0700

    clients can be kill now

--- server.py
+++ server.py
@@ -33,19 +33,21 @@ class Server:
     def handle_client(self, client, addr, player_id):
         print(f'new client {addr}, {player_id}')
         while True:
+            #  be suicidal
+            if client.fileno() == -1:
+                self.clients.remove(client)
+                self.game.remove_player(player_id)
+                client.close()
+                print('client disconnected')
+                break
+
             time.sleep(1/60)
             data = client.recv(1024*32)
             if data:
                 data = pickle.loads(data)
                 self.game.handle_input(player_id, data)
                 client.send(pickle.dumps(self.game.serialize(), pickle.HIGHEST_PROTOCOL))
-                    
-            #    self.clients.remove(client)
-            #    self.game.remove_player(player_id)
-            #    client.close()
-            #    print('client disconnected')
-            #    break
-
+                       
 if __name__ == '__main__':
     # server = Server('localhost', 9999)
     server = Server('0.0.0.0', 9999)
